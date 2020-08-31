78fa009d35d577af3a836dbeb8d012d68c472f53
user6053038 <779357914@qq.com>
2020-08-31 19:17:54 +0800

    HelloGit第一次提交。

---
+++ day01/src/main/java/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Dennis
+ * @Date: 2020/08/31/18:56
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
