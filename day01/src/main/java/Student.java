ef5f1d42a0759f9beac1990d7a8e9dbadf577810
user6053038 <779357914@qq.com>
2020-08-31 20:03:58 +0800

    添加student实体类

---
+++ day01/src/main/java/Student.java
@@ -0,0 +1,45 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Dennis
+ * @Date: 2020/08/31/20:00
+ * @Description:
+ */
+public class Student {
+    String name;
+    int age;
+    String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
