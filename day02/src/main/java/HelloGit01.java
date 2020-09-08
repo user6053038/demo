1cbe02b6fb7bdfc78d6898e900c6f3fe75dc0a46
user6053038 <779357914@qq.com>
2020-09-08 08:59:51 +0800

    添加Demo

---
+++ day02/src/main/java/HelloGit01.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Dennis
+ * @Date: 2020/09/08/8:50
+ * @Description:
+ */
+public class HelloGit01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
